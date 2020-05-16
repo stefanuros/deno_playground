249fe95fb2adcbaf9ed98b2734c3f4cd58656c3a
Stefan Urosevic <stefanuros@gmail.com>
2020-05-16 02:08:13 -0400

    Added second file

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+import { str } from "./test_string.ts";
+
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({ body: "Hello World\n" + str });
 }
